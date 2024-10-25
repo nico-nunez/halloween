0012820581d8a4e7f40439f625afdb76ced3656d
Nico Nunez <incognico.dev@gmail.com>
2024-10-25 18:40:27 -0500

    Update halloween.py

--- halloween.py
+++ halloween.py
@@ -53,4 +53,4 @@ def action(deviceName):
     return redirect('/', code=303)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
